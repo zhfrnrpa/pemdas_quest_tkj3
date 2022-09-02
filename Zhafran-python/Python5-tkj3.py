7c0662d36cfa0b27ed6b631aafed0c5e78b6f57d
zhfrnrpa <112366745+zhfrnrpa@users.noreply.github.com>
2022-09-01 20:15:14 -0700

    Zhafran TKJ 3
    Tugas Pemdas Python5

---
+++ Zhafran-python/Python5-tkj3.py
@@ -0,0 +1,57 @@
+'''Bangun datar
+    - Persegi
+    - Segitiga
+    - Jajargenjang
+    - Lingkaran'''
+
+
+# Rumus Persegi Sisi * Sisi
+
+sisi1 = 10
+sisi2 = 10
+luas_p = sisi1 * sisi2
+
+print("luas persegi adalah" , luas_p)
+
+
+# Rumus Segitiga A * T / 2
+
+alas_s = 20
+tinggi_s = 10
+luas_s = alas_s * tinggi_s / 2
+
+print("luas segitiga adalah" , luas_s)
+
+
+# Rumus Luas Jajar Genjang A * T
+
+alas_j = 10
+tinggi_j = 15
+luas_j = alas_j * tinggi_j
+
+print(" luas jajar genjang adalah" , luas_j)
+
+
+# Rumus Luas Lingkaran phi * r *
+
+phi = 22/7
+r = 14
+luas_l = phi * r * r
+
+print(" luas lingkaran adalah" , luas_l)
+
+
+'''Programan Dinamis'''
+
+print("\n1.luas persegi")
+sisi = int(input("masukan nilai sisi: "))
+luas_p = sisi * sisi
+print("luas persegi adalah" , luas_p)
+
+
+
+
+
+
+
+ 
