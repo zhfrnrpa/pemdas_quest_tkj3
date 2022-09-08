8a8c305d01d3680b38c59a536cf15dc6a6b110c9
zhfrnrpa <112366745+zhfrnrpa@users.noreply.github.com>
2022-09-07 23:38:28 -0700

    Add files via upload

---
+++ python-latihan.py
@@ -0,0 +1,31 @@
+# programan dinamis
+
+print("\n1. luas persegi")
+sisi = int(input("masukan nilai sisi: "))
+luas_p = sisi * sisi
+print("luas persegi adalah" , luas_p)
+
+print("\n2.luas segitiga")
+alas = int(input("masukan nilai alas: "))
+tinggi = int(input("masukan nilai tinggi: "))
+luas_s = alas * tinggi / 2
+print("nilai luas segitiga adalah" , luas_s)
+
+print("\n3. luas jajargenjang")
+alas = int(input("masukan nilai alas: "))
+tinggi = int(input("masukan nilai tinggi: "))
+luas_j = alas * tinggi 
+print("nilai luas jajargenjang adalah", luas_j)
+
+print("\n4 luas lingkaran")
+phi = int(input("masukan nilai phi: "))
+r1 = int(input("masukan nilai r1: "))
+r2 = int(input("masukan nilai r2: "))
+luas_l = phi * r1 * r2
+print("nilai luas jajargenjang adalah", luas_l)
+
+print =("\n5 persegi panjang")
+panjang = int(input("masukan nilai panjang: "))
+lebar = int(input("masukan nilai lebar: "))
+luas_pj = panjang * lebar 
+print("nilai luas persegi panjang adalah", luas_pj)
